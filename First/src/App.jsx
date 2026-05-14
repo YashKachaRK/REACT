db204abfdccb354c82925f6848e3b4b26b1eb124
YashKachaRK <ykacha243@rku.ac.in>
2026-05-14 15:34:37 +0530

    Learn React

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -1,10 +1,12 @@
-
 import Navbar from "./componets/Navbar";
+import Textform from "./componets/Textform";
 function App() {
   return (
     <div>
-      <Navbar title={"Textutils"}/>
-    
+      <Navbar title={"Textutils"} />
+      <div className="container">
+        <Textform />
+      </div>
     </div>
   );
 }
---
+++ First/src/componets/Textform.jsx
@@ -0,0 +1,53 @@
+import  { useState } from "react";
+
+export default function Textform() {
+  const [text, setText] = useState("");
+
+
+  const handleUpClick = () => {
+    console.log("Upper Case");
+    setText(text.toUpperCase());
+  };
+
+  const handleLowwerClick = ()=>{
+    setText(text.toLowerCase());
+  }
+
+  const handleCount = () =>{
+    setText(text.length)
+  }
+
+  const handleOnchage = (event) => {
+    console.log("OnChange");
+    setText(event.target.value);
+  };
+
+
+  return (
+    <div>
+      <div className="form-group">
+        <label for="exampleFormControlTextarea1">
+          Enter Text and select button{" "}
+        </label>
+        <textarea
+          className="form-control"
+          value={text}
+          id="myBox"
+          rows="3"
+          onChange={handleOnchage}
+        ></textarea>
+      </div>
+
+      <button className="btn btn-primary mt-3" onClick={handleUpClick}>
+        Convert to Upprcase
+      </button>
+      <button className="btn btn-primary mt-3 ms-3" onClick={handleLowwerClick}>
+        Convert to LowerCase
+      </button>
+       <button className="btn btn-primary mt-3 ms-3" onClick={handleCount}>
+        Count Length
+      </button>
+
+    </div>
+  );
+}
