e25504919e7253f5fd46e6eae8c836eff2604e17
YashKachaRK <ykacha243@rku.ac.in>
2026-05-18 17:16:49 +0530

    Learn React

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -1,22 +1,33 @@
 import About from "./componets/About";
+import Alret from "./componets/Alret";
 import Navbar from "./componets/Navbar";
 import Textform from "./componets/Textform";
 import { useState } from "react";
 
 function App() {
   const [darkmode, setDarkmode] = useState("light");
-  const [text, settext] = useState("Enable Dark Mode")
-  const toggleSwitch = () =>{
-    if (darkmode == "light"){
-      setDarkmode("dark")
-      settext("Enable Light Mode")
-    }else{
-      setDarkmode("light")
-      settext("Enable Dark Mode")
+  const [text, settext] = useState("Enable Dark Mode");
+  const toggleSwitch = () => {
+    if (darkmode == "light") {
+      setDarkmode("dark");
+      settext("Enable Light Mode");
+      document.body.style.backgroundColor = "#435c74";
+      document.body.style.color ="white"
+    } else {
+      setDarkmode("light");
+      settext("Enable Dark Mode");
+      document.body.style.backgroundColor = "white";
+            document.body.style.color ="black"
+
     }
-  }
+
+
+  };
+  
   return (
     <div>
+      <Navbar title={"Textutils"} mode={darkmode} />
+      <Alret alert="This is alert" />
       <div className="form-check form-switch">
         <input
           className="form-check-input"
@@ -26,10 +37,9 @@ function App() {
           onClick={toggleSwitch}
         ></input>
         <label className="form-check-label" for="switchCheckDefault">
-         {text}
+          {text}
         </label>
       </div>
-      <Navbar title={"Textutils"} mode={darkmode} />
       <div className="container">
         <Textform />
         {/* <About /> */}
---
+++ First/src/componets/Alret.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+export default function Alret(props) {
+  return (
+    <div>
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong> {props.alert}</strong>
+        <button
+          type="button"
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
+  );
+}
