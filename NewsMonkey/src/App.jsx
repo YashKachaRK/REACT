529efb0e8168567e85ff0276f5cfcf30e928d3af
YashKachaRK <ykacha243@rku.ac.in>
2026-05-19 17:36:10 +0530

    Learn React

---
+++ NewsMonkey/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+// method make here
+// propse state use method diffrent
+// 5da591db591a4c2488c42a12b2bef658
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Class Base Component</p>
+      </div>
+    )
+  }
+}
