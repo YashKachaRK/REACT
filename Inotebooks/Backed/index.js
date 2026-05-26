264806d17eeb4e0e48dc238f82c7f28476e37903
YashKachaRK <ykacha243@rku.ac.in>
2026-05-26 14:31:56 +0530

    Lear React

--- Inotebooks/Backed/index.js
+++ Inotebooks/Backed/index.js
@@ -1,1 +1,22 @@
-// backed entry app 
+// backed entry point 
+const connectToMongo = require('./db');
+const express = require('express');
+require('dotenv').config();
+
+connectToMongo()
+
+const app = express();
+const port = process.env.PORT ;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Hello World!' );
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
