46daf52939dae0dd298e8148760d9c2c3f69eb97
YashKachaRK <ykacha243@rku.ac.in>
2026-05-28 16:49:24 +0530

    Learn react

---
+++ Inotebooks/Notebook/src/context/notes/NoteState.js
@@ -0,0 +1,13 @@
+import noteContext from "./NoteContext";
+
+const NoteState = (props)=>{
+  return(
+
+    <NoteState.provider>
+      {props.children}
+    </NoteState.provider>
+  )
+}
+
+export default NoteState;
+ 
