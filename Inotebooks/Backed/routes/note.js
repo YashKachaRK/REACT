e4a7ed58ffd081209c983ff1d8f8fb99cb929517
YashKachaRK <ykacha243@rku.ac.in>
2026-05-28 11:26:38 +0530

    Learn react

--- Inotebooks/Backed/routes/note.js
+++ Inotebooks/Backed/routes/note.js
@@ -98,4 +98,47 @@ router.put('/updatenote/:id',
       res.status(500).send("Server Error");
     }
   })
+
+// route 3 :  delete notes POST : "/api/note/addnotes"
+router.delete('/deletenote/:id',
+  fatchuser,
+  async (req, res) => {
+    try {
+
+     
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+
+
+     
+
+      // note id check kari chhe ahiya
+      let  note =await  Notes.findById(req.params.id);
+      if (!note) {
+        return  res.status(404).json("Not Found")
+      }
+
+      // user check kari chhi // Check ownership
+
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).json("Now Allowed")
+      }
+
+      note = await Notes.findByIdAndDelete(req.params.id)
+
+      res.json ({" Success" : "Note Hase been deleted"})
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server Error");
+    }
+  })
+
+
+
 module.exports = router
