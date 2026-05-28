65269bf1f15eee396cc2727ca1440794d2fc5f91
YashKachaRK <ykacha243@rku.ac.in>
2026-05-28 17:48:20 +0530

    Learn react

--- Inotebooks/Notebook/src/components/Notes.jsx
+++ Inotebooks/Notebook/src/components/Notes.jsx
@@ -1,44 +1,55 @@
 import { Pencil, Trash2, Tag } from "lucide-react";
+import { useContext } from "react";
+import noteContext from "../context/notes/NoteContext";
 
 export default function Notes() {
-  return (
-    <div className="bg-zinc-900 border border-zinc-800 rounded-2xl p-5 shadow-lg hover:scale-[1.02] transition duration-300 m-10">
-      {/* Top */}
-      <div className="flex items-start justify-between">
-        <div>
-          <h2 className="text-2xl font-bold text-white">React Notes</h2>
+  const context = useContext(noteContext);
+  const { note, setNote } = context;
 
-          <div className="flex items-center gap-2 mt-2 text-yellow-400 text-sm">
-            <Tag size={16} />
-            <span>Study</span>
+  return (
+    <div>
+      {note.map((n) => {
+        return n.title;
+      })}
+      <div className="bg-zinc-900 border border-zinc-800 rounded-2xl p-5 shadow-lg hover:scale-[1.02] transition duration-300 m-10">
+        {/* Top */}
+
+        <div className="flex items-start justify-between">
+          <div>
+            <h2 className="text-2xl font-bold text-white">React Notes</h2>
+
+            <div className="flex items-center gap-2 mt-2 text-yellow-400 text-sm">
+              <Tag size={16} />
+              <span>Study</span>
+            </div>
+          </div>
+
+          {/* Action Buttons */}
+          <div className="flex gap-3">
+            <button className="bg-zinc-800 p-2 rounded-lg hover:bg-yellow-400 hover:text-black transition duration-300">
+              <Pencil size={18} />
+            </button>
+
+            <button className="bg-zinc-800 p-2 rounded-lg hover:bg-red-500 hover:text-white transition duration-300">
+              <Trash2 size={18} />
+            </button>
           </div>
         </div>
 
-        {/* Action Buttons */}
-        <div className="flex gap-3">
-          <button className="bg-zinc-800 p-2 rounded-lg hover:bg-yellow-400 hover:text-black transition duration-300">
-            <Pencil size={18} />
-          </button>
+        {/* Description */}
+        <p className="text-zinc-400 mt-5 leading-relaxed">
+          Learn Context API, React Router, Tailwind CSS, authentication system
+          and backend integration using MERN stack project.
+        </p>
 
-          <button className="bg-zinc-800 p-2 rounded-lg hover:bg-red-500 hover:text-white transition duration-300">
-            <Trash2 size={18} />
+        {/* Footer */}
+        <div className="flex items-center justify-between mt-6">
+          <span className="text-xs text-zinc-500">28 May 2026</span>
+
+          <button className="text-yellow-400 text-sm hover:underline">
+            Read More
           </button>
         </div>
-      </div>
-
-      {/* Description */}
-      <p className="text-zinc-400 mt-5 leading-relaxed">
-        Learn Context API, React Router, Tailwind CSS, authentication system and
-        backend integration using MERN stack project.
-      </p>
-
-      {/* Footer */}
-      <div className="flex items-center justify-between mt-6">
-        <span className="text-xs text-zinc-500">28 May 2026</span>
-
-        <button className="text-yellow-400 text-sm hover:underline">
-          Read More
-        </button>
       </div>
     </div>
   );
--- Inotebooks/Notebook/src/context/notes/NoteState.jsx
+++ Inotebooks/Notebook/src/context/notes/NoteState.jsx
@@ -1,30 +1,60 @@
+import { useState } from "react";
 import noteContext from "./NoteContext";
+
 // import { useState } from "react";
 const NoteState = (props) => {
 
 
-  const s1 = {
-    name: "Yash",
-    class: "A",
-  };
+  const notesIntial = [
+    {
+      _id: "6a17c93320f7d37ae0451399",
+      user: "6a16decceb42c3b5ae8ba7d4",
+      title: "My Notes Good Working ",
+      description: "My notes is good",
+      tag: "Persnol",
+      date: "2026-05-28T04:48:51.227Z",
+      __v: 0,
+    },
+    {
+      _id: "6a17c9b020f7d37ae045139a",
+      user: "6a16decceb42c3b5ae8ba7d4",
+      title: "My Notes",
+      description: "My notes is good",
+      tag: "Persnol",
+      date: "2026-05-28T04:50:56.261Z",
+      __v: 0,
+    },
+    {
+      _id: "6a17c9b120f7d37ae045139b",
+      user: "6a16decceb42c3b5ae8ba7d4",
+      title: "My Notes",
+      description: "My notes is good",
+      tag: "Persnol",
+      date: "2026-05-28T04:50:57.221Z",
+      __v: 0,
+    },
+  ];
 
-
-  // const [state, setState] = useState(s1);
-  // const update = () => {
-  //   setTimeout(() => {
-  //     setState({
-  //       name: "gracy",
-  //       class: "b",
-  //     });
-  //   }, 1000);
-  // };
-
+  const [note, setNote] = useState(notesIntial)
 
   return (
-    <noteContext.Provider value={s1}>{props.children}</noteContext.Provider>
+    <noteContext.Provider value={{note , setNote}}>{props.children}</noteContext.Provider>
   );
-
-
 };
 
 export default NoteState;
+// const [state, setState] = useState(s1);
+// const update = () => {
+//   setTimeout(() => {
+//     setState({
+//       name: "gracy",
+//       class: "b",
+//     });
+//   }, 1000);
+// };
+
+
+  // const s1 = {
+  //   name: "Yash",
+  //   class: "A",
+  // };
